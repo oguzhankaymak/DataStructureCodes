358faed01aa38fde41f19752a7dca3984c9bc9af
oguzhankaymak <oguzhankaymakdev@gmail.com>
2020-04-05 02:11:52 +0300

    Experimental Analysis added

---
+++ DeneyselAnaliz/ExperimentalAnalysis/ExperimentalAnalysis/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentalAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            string number = "";
+            for (int i = 0; i < 100000; i++)
+            {
+                number += i.ToString();
+            }
+
+            watch.Stop();
+            
+            Console.WriteLine("Çalışma Süresi: {0}", watch.Elapsed.Milliseconds);
+            Console.ReadKey();
+
+            // -------------------------------------------------------------------//
+            //Stopwatch watch = new Stopwatch();
+            //watch.Start();
+            //StringBuilder stringBuilder = new StringBuilder();
+            
+
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    stringBuilder.Append(i.ToString());
+            //}
+
+            //watch.Stop();
+
+            //Console.WriteLine("Çalışma Süresi: {0}", watch.Elapsed.Milliseconds);
+            //Console.ReadKey();
+
+        }
+    }
+}
